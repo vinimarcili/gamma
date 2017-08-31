026f858a9f07b5373730d100b8cd7d3e2666823a
vinimarcili <vinipetter@gmail.com>
2017-08-31 10:30:44 -0300

    Adicionando Sass, Bootstrap e Font Awaesome

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import App from './App'
 import router from './router'
+
+/* CSS bootstrap files */
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+/* FontAwesome files */
+import 'font-awesome/css/font-awesome.css'
+
+/* Import Sass Global File */
+import '../src/assets/scss/global.scss'
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
