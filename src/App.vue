4c543e3b26f7cadf0f06e7f6cc461d07c9584e2d
vinimarcili <vinipetter@gmail.com>
2017-09-01 09:32:23 -0300

    Base Design

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <div id="app">
+  <div id="app" class="default-font">
     <figure>
       <img src="./assets/images/logo.png" alt="Gamma" title="Gamma" class="img-responsive">
     </figure>
     <router-view></router-view>
+    <footer>
+      <a href="mailto:vinipetter@gmail.com">Developed By Vinicius Marcili</a>
+      <a href="mailto:saragarcia.gr@gmail.com">Base Design By Sara Garcia</a>
+    </footer>
   </div>
 </template>
 
@@ -14,15 +18,32 @@ export default {
 </script>
 
 <style lang="scss">
+$transition: all 0.3s ease;
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding: 50px 0 15px;
+  background: rgba(0, 0, 0, 0.9);
   figure{
-    margin: 0;
+    max-width: 300px;
+    display: inline-block;
+    margin: 0 auto;
+  }
+  footer {
+    a {
+      display: block;
+      color: #fff;
+      transition: $transition;
+      font-size: 15px;
+      &:hover {
+        text-decoration: none;
+        font-size: 17px;
+      }
+    }
   }
 }
 </style>
--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,6 +1,71 @@
 <template>
-  <div class="calculator">
-
+  <div class="calculator default-font">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <p class="instructions">
+            Mussum Ipsum, cacilds vidis litro abertis. In elementis mé pra quem é amistosis quis leo. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Copo furadis é disculpa de bebadis, arcu quam euismod magna. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi.
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <p class="float-text">
+            Select gender
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-lg-offset-3">
+          <input type="radio" name="gender" id="male">
+          <label for="male">
+            <i class="fa fa-male" aria-hidden="true"></i>
+          </label>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+          <input type="radio" name="gender" id="female">
+          <label for="female">
+            <i class="fa fa-female" aria-hidden="true"></i>
+          </label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+          <label for="age" class="hidden">
+            Age
+          </label>
+          <input type="text" name="age" id="age" placeholder="Age">
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+          <label for="time" class="hidden">
+            Training time (minutes)
+          </label>
+          <input type="text" name="time" id="time" placeholder="Training time (minutes)">
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+          <label for="weight" class="hidden">
+            Weight (kg)
+          </label>
+          <input type="text" name="weight" id="weight" placeholder="Weight (kg)">
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+          <label for="height" class="hidden">
+            Height (cm)
+          </label>
+          <input type="text" name="height" id="height" placeholder="Height (cm)">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <button type="button">Calculate <i class="fa fa-check" aria-hidden="true"></i></button>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <p class="result">Result: $var Kcal</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,7 +80,102 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
+  $transition: all 0.3s ease;
 
+  $green: rgba(20, 220, 20, 0.4);
+  $green-light: rgba(50, 205, 50, 0.5);
+
+  .calculator {
+    p {
+      margin: 15px auto;
+      display: inline-block;
+      &.float-text{
+        margin: 15px auto 5px;
+      }
+      &.instructions {
+        max-width: 500px;
+        background: $green;
+        border-radius: 10px;
+        padding: 15px;
+      }
+      &.result {
+        max-width: 500px;
+        background: $green-light;
+        border-radius: 10px;
+        padding: 15px;
+      }
+    }
+    input {
+      &:focus {
+        outline: none;
+      }
+      &[type="radio"] {
+        position: absolute;
+        left: -9999px;
+        & + label {
+          cursor: pointer;
+          background: $green;
+          width: 50px;
+          height: 50px;
+          border: 1px solid $green-light;
+          border-radius: 50%;
+          line-height: 50px;
+          font-size: 35px;
+          margin: 15px;
+          color: #fff;
+          transition: $transition;
+          &:hover{
+            background: #fff;
+            color: $green-light;
+          }
+        }
+        &:not(:checked) {
+          & + label {
+            border: none;
+          }
+        }
+        &:checked {
+          & + label {
+            background: darken($green, 20%);
+            color: #fff;
+            border-width: 3px;
+            line-height: 47px;
+          }
+        }
+      }
+      &[type="text"] {
+        transition: $transition;
+        max-width: 500px;
+        width: 100%;
+        height: 40px;
+        background: $green;
+        border: none;
+        border-radius: 5px;
+        margin: 15px auto;
+        color: #fff;
+        padding: 0 15px;
+        font-size: 20px;
+        border: 1px solid rgba(0, 0, 0, 0);
+        &:hover, &:focus {
+          border-color: #fff;
+        }
+        &::placeholder {
+          color: #fff;
+        }
+      }
+    }
+    button {
+      border: none;
+      max-width: 500px;
+      background: $green-light;
+      border-radius: 10px;
+      padding: 15px;
+      transition: $transition;
+      &:hover{
+        background: #fff;
+        color: darken($green, 20%);
+      }
+    }
+  }
 </style>
